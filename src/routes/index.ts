4317e70425ce94dcdabd1ea1529c5e2bb43ec4db
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-27 03:28:00 +0600

    added

---
+++ src/routes/index.ts
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+// import authRouter from '../app/modules/auth/auth.route';
+// // import postPreferenceRouter from '../app/modules/PostPreference/postPreference.route';
+// import messageRoute from '../app/modules/message/message.route';
+// import userRouter from '../app/modules/user/user.route';
+// import medicineRouter from '../app/modules/addMedicine/addMedicine.route';
+// import orderRouter from '../app/modules/order/order.route';
+// import doctorRoute from '../app/modules/doctor/doctor.route';
+// import needMedicineRoute from '../app/modules/needMedicine/needMedicine.route';
+// import helpRoute from '../app/modules/help/help.route';
+
+
+
+const router = Router();
+
+const moduleRoutes = [
+    {
+    path: '/auth',
+    route: authRouter,
+    },
+    // {
+    //   path: '/addMedicine',
+    //   route: medicineRouter,
+    // },
+    // {
+    //   path: '/doctor',
+    //   route: doctorRoute,
+    // },
+    // {
+    //   path: '/order',
+    //   route: orderRouter,
+    // },
+    // {
+    //   path: '/message',
+    //   route: messageRoute,
+    // },
+    // {
+    //   path: '/user',
+    //   route: userRouter,
+    // },
+    // {
+    //   path: '/needMedicine',
+    //   route: needMedicineRoute,
+    // },
+    // {
+    //   path: '/help',
+    //   route: helpRoute,
+    // },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
