c10cef83de816ea3178275c813eec3edd4a210a6
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-27 03:17:44 +0600

    added

--- src/errors/handleCastError.ts
+++ src/errors/handleCastError.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { TErrorSources, TGenericErrorResponse } from '../interface/error';
 
 
+
 const handleCastError = (
   err: mongoose.Error.CastError,
 ): TGenericErrorResponse => {
